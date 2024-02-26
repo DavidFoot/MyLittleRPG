13929a21e66f68d3bd4980c9c36a42ee7f736c79
DavidFoot <48476987+DavidFoot@users.noreply.github.com>
2024-02-26 10:04:33 +0100

    Add portal with Spawnpoint

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 
 namespace RPG.SceneManagement
@@ -8,7 +10,12 @@ namespace RPG.SceneManagement
     public class Portal : MonoBehaviour
     {
         [SerializeField] int sceneToLoad;
-        // Start is called before the first frame update
+        enum DestinationIdentifier
+        {
+            A, B, C, D, E
+        }
+        [SerializeField] DestinationIdentifier destination;
+        [SerializeField] Transform spawnPoint;
         public void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Player")
@@ -21,7 +28,31 @@ namespace RPG.SceneManagement
             print("Enter in a new scene");
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
+            // Trouver comment apparaitre sur le spawnpoint du portail suivant
+            Portal destinationPortal = GetDestinationPortal();
+            Debug.Log("Destination : " + destinationPortal.destination);
+            // Placer le player sur le spawnPoint du portal
+            UpdatePlayer(destinationPortal);
             print("scene loaded");
+            Destroy(gameObject);
+        }
+        private void UpdatePlayer(Portal portal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().enabled = false;
+            player.transform.position = portal.spawnPoint.transform.position;
+            player.transform.rotation = portal.spawnPoint.transform.rotation; 
+            player.GetComponent<NavMeshAgent>().enabled = true;
+        }
+        private Portal GetDestinationPortal()
+        {
+            foreach(Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if (portal.destination != this.destination) continue;
+                return portal;
+            }
+            return null;
         }
     }
 
